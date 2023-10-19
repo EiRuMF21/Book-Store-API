4a83b72d2ccaaac6817951880c3ca36c2bf72867
EiRuMF21 <imffarrel@gmail.com>
2023-10-19 10:59:25 +0700

    inisialisasi

--- index.js
+++ index.js
@@ -2,10 +2,46 @@ const express = require('express')
 
     const app = express()
     const port = 2323
+
+    const data_users = [
+        { id: 1, name: "Iurie Muhamad", alamat: "Ujungberung" },
+        { id: 2, name: "Arya Abdullah", alamat: "Cimahi" },
+        { id: 3, name: "Aji Permana", alamat: "Kircon" },
+        { id: 4, name: "Azka Dafin", alamat: "Antapani" },
+    ];
+    app.get("/users/:id", (req, res) => {
+       
+        let id = parseInt(req.params.id);
+
+        let result;
+        const user = data_users.find((user) => user.id === id);
+        if (user) {
+            result = {
+            status: 200,
+            data: user,
+            };
+        } else {
+            res.status(404).json({ error: "User not found" });
+        }
+        res.json(result);
+    });
     
-    app.get('/Kopi latte',(req,res) =>{
-        res.json("Ini kopi latte ")
-    })
+    app.get("/users/:id", (req, res) => {
+        let id = parseInt(req.params.id);
+
+        
+        let result;
+        const user = data_users.find((user) => user.id === id);
+        if (user) {
+            result = {
+            status: 200,
+            data: user,
+            };
+        } else {
+            res.status(404).json({ error: "User not found" });
+        }
+        res.json(result);
+    });
     app.post('/Kopi Mochacino',(req,res) =>{
         res.json("Ini kopi Mochacino ")
     })
